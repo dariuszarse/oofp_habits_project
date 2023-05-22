6d075e46b9d8e0fedb54b67d45cc8cae4b5936d9
dariuszarse <114665449+dariuszarse@users.noreply.github.com>
2023-05-22 19:38:11 +0200

    Add files via upload

--- essential files/tests.py
+++ essential files/tests.py
@@ -1,11 +1,13 @@
 import unittest
 import sqlite3
+import datetime
+
 
 class Test(unittest.TestCase):
     def test_insert_dummy_data(self):
-        '''this test succeeds if and only if the table HABITS in the database habits.db is successfully filled with 5 habits (the dummy data)'''
+        '''this test checks the number of rows in the table habits in the database habits.db.
+        the test succeeds only when the table successfully filled with 5 habits (the dummy data).'''
         connection = sqlite3.connect('habits.db')
-        self.assertIsNot(connection, 0)
         cursor = connection.cursor()
         cursor.execute('Select HABIT_NAME from HABITS')
         results = cursor.fetchall()
@@ -13,9 +15,16 @@ class Test(unittest.TestCase):
         cursor.close()
     
     def test_streak_broken(self):
-        '''dummy data includes habits which are still marked with active streaks but an attribute check_off_last that is older than today.
-        this test succeeds if the current streak is successfully set to 0'''
-
+        '''this test checks for all habits and their check_off_next attribute. 
+        if the date stored in the attribute is older than today, it checks if the attribute habit_current_streak has been set to 0.
+        the test succeeds if this is true for all instances.'''
+        connection = sqlite3.connect('habits.db')
+        cursor = connection.cursor()
+        today = datetime.datetime.today().date()
+        cursor.execute('Select HABIT_CURRENT_STREAK from HABITS where HABIT_CHECK_OFF_NEXT < date(?) = TRUE', (today,))
+        results = cursor.fetchall()
+        self.assertTrue(x=='0,' for x in results)
+        cursor.close()
 
 if __name__ == '__main__':
     unittest.main()
